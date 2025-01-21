4a1ccde81875bc18ad638bbae3bde94a9ba1f385
Abhay Ramesh <ramesh.abhay14@gmail.com>
2025-01-21 18:04:46 +0530

    removing server envs and certs

--- frontends/base/nuxt.config.ts
+++ frontends/base/nuxt.config.ts
@@ -1,6 +1,3 @@
-import fs from 'node:fs'
-import process from 'node:process'
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: true,
@@ -11,70 +8,68 @@ export default defineNuxtConfig({
     },
   },
   routeRules: {
-    '/': { prerender: true },
-    '/why-influencers': { prerender: true },
-    '/pricing': { prerender: true },
-    '/how-it-works': { prerender: true },
+    "/": { prerender: true },
+    "/why-influencers": { prerender: true },
+    "/pricing": { prerender: true },
+    "/how-it-works": { prerender: true },
   },
   modules: [
-    '@nuxt/ui',
-    'nuxt-lodash',
-    '@pinia/nuxt',
-    '@unlazy/nuxt',
-    '@vueuse/nuxt',
-    '@nuxtjs/google-fonts',
+    "@nuxt/ui",
+    "nuxt-lodash",
+    "@pinia/nuxt",
+    "@unlazy/nuxt",
+    "@vueuse/nuxt",
+    "@nuxtjs/google-fonts",
   ],
   lodash: {
-    alias: [
-      ['get', 'getByPath'],
-    ],
+    alias: [["get", "getByPath"]],
   },
   colorMode: {
-    preference: 'light',
+    preference: "light",
   },
   components: {
     dirs: [
       {
-        path: '~/components',
+        path: "~/components",
         pathPrefix: false,
       },
     ],
   },
   imports: {
-    dirs: ['interfaces/**/**', 'composables/**/**', 'store'],
+    dirs: ["interfaces/**/**", "composables/**/**", "store"],
   },
   ui: {
-    icons: ['heroicons', 'simple-icons'],
+    icons: ["heroicons", "simple-icons"],
   },
   runtimeConfig: {
     public: {
-      customerDomain: '',
-      adminDomain: '',
+      customerDomain: "",
+      adminDomain: "",
     },
   },
-  vite: {
-    server: process.env.APP_ENV === 'production'
-      ? undefined
-      : {
-          https: {
-            key: fs.readFileSync(`../../certs/${process.env.NUXT_PUBLIC_APP_DOMAIN}.key`),
-            cert: fs.readFileSync(`../../certs/${process.env.NUXT_PUBLIC_APP_DOMAIN}.crt`),
-          },
-          hmr: {
-            clientPort: 443,
-          },
-        },
-  },
+  // vite: {
+  //   server: process.env.APP_ENV === 'production'
+  //     ? undefined
+  //     : {
+  //         https: {
+  //           key: fs.readFileSync(`../../certs/${process.env.NUXT_PUBLIC_APP_DOMAIN}.key`),
+  //           cert: fs.readFileSync(`../../certs/${process.env.NUXT_PUBLIC_APP_DOMAIN}.crt`),
+  //         },
+  //         hmr: {
+  //           clientPort: 443,
+  //         },
+  //       },
+  // },
   googleFonts: {
     families: {
       Inter: [400, 500, 700],
     },
-    display: 'swap',
+    display: "swap",
     download: true,
     inject: true,
-    text: 'Why Influencers? Pricing How it Works Book A Demo Login Reach new audiences Boost restaurant traffic with local food influencers Restaurant marketing redefined for the social media age. Try it for free Learn More 270% 1.1M 15.6K Get matched with local, trusted food influencers. Ready to post about your business',
+    text: "Why Influencers? Pricing How it Works Book A Demo Login Reach new audiences Boost restaurant traffic with local food influencers Restaurant marketing redefined for the social media age. Try it for free Learn More 270% 1.1M 15.6K Get matched with local, trusted food influencers. Ready to post about your business",
   },
   pinia: {
-    storesDirs: ['./store/**'],
+    storesDirs: ["./store/**"],
   },
-})
+});
